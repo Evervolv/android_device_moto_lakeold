Jleeblanch/android_device_motorola_lake

lake: enable retrofit dynamic partitions
Initial change to switch to dynamic partitions for sdm660

Change-Id: If8002c767c0e59592463ca7c0a46db99007e1653

device.mk
@@ -69,6 +69,10 @@ PRODUCT_BOOT_JARS += qcom.fmradio
 PRODUCT_PACKAGES += \
     init.lake.rc
 
+PRODUCT_COPY_FILES += \
+    device/motorola/sdm660-common/rootdir/etc/rdp_fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    device/motorola/sdm660-common/rootdir/etc/rdp_fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \

Evervolv/android_device_moto_lakeold

device.mk
@@ -63,6 +63,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.lake.rc
 
+PRODUCT_COPY_FILES += \
+    device/moto/sdm660-common/rootdir/etc/rdp_fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    device/moto/sdm660-common/rootdir/etc/rdp_fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \