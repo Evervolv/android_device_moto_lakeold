1649f3953e550788c21b7100b61bea2d7e220d5b
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-28 23:46:55 -0400

    lake: Prevent UHID group addition from multiplying on re-extract
    Change-Id: I0ef3c01b9416b0c5293308bc5d1a898c2f4cd3c4

--- extract-files.sh
+++ extract-files.sh
@@ -15,6 +15,19 @@
 # limitations under the License.
 #
 
+function blob_fixup() {
+    case "${1}" in
+        # Add uhid group for fingerprint service
+        vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service-ets.rc)
+            sed -i "s/system input/system uhid input/" "${2}"
+            ;;
+        # Fix camera recording
+        vendor/lib/libmmcamera2_pproc_modules.so)
+            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
