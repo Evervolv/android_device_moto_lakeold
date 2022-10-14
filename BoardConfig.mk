PixelExperience-Devices/device_motorola_lake

lake: Add board-info.txt
* Fixes flashall in fastbootd

Change-Id: Iae2f33113317cfaadeff7a3610463880077c35e0

BoardConfig.mk
@@ -31,6 +31,7 @@ AB_OTA_PARTITIONS += \
     vendor
 
 # Assertions
+TARGET_BOARD_INFO_FILE := device/motorola/lake/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := lake,lake_n
 
 # Display

Evervolv/android_device_moto_lakeold

BoardConfig.mk
@@ -31,6 +31,7 @@ AB_OTA_PARTITIONS += \
     vendor
 
 # Assertions
+TARGET_BOARD_INFO_FILE := device/moto/lake/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := lake,lake_n
 
 # Display